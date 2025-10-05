cc3ec0255ef51b25d7aa1e467d0736b61398c1bf
Joshua Jude Wuebbolt <joshua.wuebbolt@mail.utoronto.ca>
2025-10-04 20:45:30 -0400

    Reworked ball shooting

--- BallBlaster/Assets/Scripts/Shooting.cs
+++ BallBlaster/Assets/Scripts/Shooting.cs
@@ -7,6 +7,7 @@ public class Shooting : MonoBehaviour
     public GameObject hitEffectPrefab;
     public float range = 50f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float shootForce = 100f;
     void Start()
     {
 
@@ -23,14 +24,20 @@ public class Shooting : MonoBehaviour
         
     }
 
-    void Shoot() {
-
+    void Shoot() 
+    {
         Vector2 pos = Mouse.current.position.ReadValue();
-        Ray ray = playerCamera.ScreenPointToRay(pos);
-        if (Physics.Raycast(ray, out var hit, range))
-        {
-            Debug.Log(hit.transform.name);
-            Instantiate(hitEffectPrefab, hit.point, Quaternion.LookRotation(hit.normal));
-        }
+        // Ray ray = playerCamera.ScreenPointToRay(pos);
+        // if (Physics.Raycast(ray, out var hit, range))
+        // {
+            // Debug.Log(hit.transform.name);
+            // Spawn the effect at the hit point, facing the surface normal
+            var create = Instantiate(hitEffectPrefab, playerCamera.transform.position + playerCamera.transform.forward * 1f, Quaternion.LookRotation(playerCamera.transform.forward));
+            Rigidbody rb = create.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.linearVelocity = playerCamera.transform.forward * shootForce;
+            }
+        // }
     }
 }
